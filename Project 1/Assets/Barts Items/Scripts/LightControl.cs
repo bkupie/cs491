485a8e631b60143f975cd9ed6a3f961d872550d4
bkupie <bkupie2@uic.edu>
2017-09-16 22:12:06 -0500

    Added lighting + Controls
    Added lighting + Controls

---
+++ Project 1/Assets/Barts Items/Scripts/LightControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+
+    public Light lt;
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1") && lt.intensity < 1.8)
+        {
+            lt.intensity+= 0.1f;
+        }
+        else if (Input.GetButton("Fire2") && lt.intensity > 0)
+        {
+            lt.intensity -= 0.1f;
+        }
+
+
+   
+    }
+}
