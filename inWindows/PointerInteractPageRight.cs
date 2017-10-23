1d9f3b3fc4cf1d7d5f0fbd9d6366643151f9c12a
p10trh <peterhanulajr@gmail.com>
2017-10-23 11:45:07 -0500

    more interaction

---
+++ inWindows/PointerInteractPageRight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class PointerInteractPageRight : VRTK_InteractableObject {
+
+    public Renderer rend;
+
+    public override void StartUsing (VRTK_InteractUse usingObject) {
+        base.StartUsing (usingObject);
+        Debug.Log ("ON");
+
+        //rend = GetComponent<Renderer> ();
+        //rend.material.shader = Shader.Find ("Specular");
+        //rend.material.SetColor ("_SpecColor", Color.red);
+    }
+
+    public override void StopUsing (VRTK_InteractUse usingObject) {
+        base.StopUsing (usingObject);
+        Debug.Log ("OFF");
+
+        //rend = GetComponent<Renderer> ();
+        //rend.material.shader = Shader.Find ("Specular");
+        //rend.material.SetColor ("_SpecColor", Color.white);
+    }
+
+    protected void Start () {
+    }
+
+    protected override void Update () {
+        base.Update ();
+    }
+}
